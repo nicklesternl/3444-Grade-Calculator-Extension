94e821a4332e8a3be08668031ff494e2ef542f9f
PeytonCoate <161971482+PeytonCoate@users.noreply.github.com>
2024-06-14 11:16:39 -0500

    Create GradeCalculation.js
    A barebones JS framework for grade calculation. Still need to figure out how to algebraically calculate required grades.

---
+++ Backend/GradeCalculation.js
@@ -0,0 +1,71 @@
+class Course {
+    constructor(name, average) {
+      this.name = name;
+      this.average = average;
+      this.assignmentTypes = [];
+    }
+
+    addAssignmentType(assignmentType) {
+        if (assignmentType instanceof AssignmentType) {
+            this.assignmentTypes.push(assignmentType);
+        } else {
+            throw new Error("Argument must be an assignmentType");
+        }
+    }
+
+    calculateAverage(){
+        let assignmentTypeAverageArray = [];
+        let assignmentWeightArray = [];
+
+        this.assignmentTypes.forEach((assignmentType) => {
+
+            const assignmentAvg = assignmentType.getAssignmentTypeAverage();
+            const assignmentWeight = assignmentType.getAssignmentWeight();
+
+            assignmentTypeAverageArray.push(assignmentAvg);
+            assignmentWeightArray.push(assignmentWeight);
+        });
+        
+        const totalWeight = assignmentWeightArray.reduce((sum, weight) => sum + weight, 0);
+        const weightedAverage = assignmentTypeAverageArray.reduce((sum, avg, index) => sum + (avg * assignmentWeightArray[index]), 0) / totalWeight;
+        return weightedAverage;
+    }
+
+}
+
+class AssignmentType {
+    constructor(name, weight) {
+      this.name = name;
+      this.weight = weight;
+      this.grades = [];
+    }
+
+    addGrade(grade) {
+        if (grade instanceof Grade) {
+            this.grades.push(grade);
+        } else {
+            throw new Error("Argument must be a grade");
+        }
+    }
+
+    getAssignmentWeight(){
+        return this.weight;
+    }
+
+    getAssignmentTypeAverage(){
+        if (this.grades.length === 0) return 0;
+
+        let total = 0;
+        for(var i = 0; i < this.grades.length; i++) {
+        total += this.grades[i].score;
+        }
+        return total / this.grades.length;
+    }
+}
+
+class Grade {
+    constructor(type, score) {
+      this.type = type;
+      this.score = score;
+    }
+}
